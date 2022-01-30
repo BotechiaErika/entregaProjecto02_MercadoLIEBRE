d846961827993d04f38cb411941cf9078328d3c2
Botechia Erika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-30 03:20:19 -0300

    add new

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// app.js in construccion
+const express = require('express')
+const app = express()
+path = require('path')
+app.use(express.static('public'));
+const resolve = path(__dirname + './public');
+
+app.get('/home' , (req, res) => {
+res.sendFile( resolve + '/views/home.html')
+})
+
+app.listen(3070, () => 
+console.log("rodando 3070")
+);
