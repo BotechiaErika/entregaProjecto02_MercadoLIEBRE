e6f4c6c4393ff9f2230fdcc6731d7753f144d41d
Botechia Erika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-30 03:36:05 -0300

    add

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 path = require('path')
 app.use(express.static('public'));
-const resolve = path(__dirname + './public');
+console.log(__dirname)
+const resolve = path.resolve(__dirname + './public');
 
 app.get('/home' , (req, res) => {
 res.sendFile( resolve + '/views/home.html')
