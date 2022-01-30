1ac873c13ea018de02d085113a11772d3db69fd7
Botechia Erika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-30 06:48:09 -0300

    app

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const app = express()
-path = require('path')
-app.use(express.static('public'));
-console.log(__dirname)
-const resolve = path.resolve(__dirname + './public');
+const express = require('express ');
+const app = express();
+const path = require('path');
+const resolve = require('resolve');
+const pathPublic = path.join(__dirname + './public');
+let pathHome = resolve(pathPublic);
+const static = app.use(express.static(pathHome));
 
-app.get('/home' , (req, res) => {
-res.sendFile( resolve + '/views/home.html')
-})
-
-app.listen(3070, () => 
-console.log("rodando 3070")
-);
+app.get('/*', (req, res) => {
+  res.sendFile(app.use(express.static(pathHome)));
+});
+app.get('/home', (req, res) => {
+  res.sendFile(pathHome + '/views/home.html')
+});
+app.listen(3030, () => {console.log('LEVANTE RUTA PORT 3030')});
